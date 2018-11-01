61b70f007a1f6b5ddc75d08a0c236c9a142d43b9
dfzamora <danielfranciscoz@hotmail.com>
2018-11-01 16:02:49 -0600

    mejoras search

--- Posgradophp/database/seeds/CursosSeeder.php
+++ Posgradophp/database/seeds/CursosSeeder.php
@@ -15,7 +15,7 @@ class CursosSeeder extends Seeder
         //Realiza insert a la tabla, esto sirve para cuando se esta creando la base de datos desde cero
         Curso::create([
             'NombreCurso'=>'Programando en Java',
-            'Image_URL'=>'img/Resources/test_img_0.png',
+            'Image_URL'=>'/img/Resources/test_img_0.png',
             'Descripcion'=>'Con este curso el estudiante podra realizar un sinnumero de estudios',
             'HorasClase'=>'6',
             'Nivel'=>'Principiante',
@@ -24,7 +24,7 @@ class CursosSeeder extends Seeder
 
         Curso::create([
             'NombreCurso'=>'Programando en Python',
-            'Image_URL'=>'img/Resources/test_img_1.png',
+            'Image_URL'=>'/img/Resources/test_img_1.png',
             'Descripcion'=>'Esta es una descripcion de prueba',
             'HorasClase'=>'25',
             'Nivel'=>'Intermedio',
@@ -33,7 +33,7 @@ class CursosSeeder extends Seeder
         
         Curso::create([
             'NombreCurso'=>'Cuso Intensivo Laravel',
-            'Image_URL'=>'img/Resources/test_img_3.png',
+            'Image_URL'=>'/img/Resources/test_img_3.png',
             'Descripcion'=>'Esta es una descripcion de prueba para el curso de laravel',
             'HorasClase'=>'12',
             'Nivel'=>'Avanzado',
@@ -42,7 +42,7 @@ class CursosSeeder extends Seeder
         
         Curso::create([
             'NombreCurso'=>'Desarrollo Web con Angular',
-            'Image_URL'=>'img/Resources/test_img_4.png',
+            'Image_URL'=>'/img/Resources/test_img_4.png',
             'Descripcion'=>'Angular, como el futuro de la web',
             'HorasClase'=>'38',
             'Nivel'=>'Avanzado',
@@ -51,7 +51,7 @@ class CursosSeeder extends Seeder
         
         Curso::create([
             'NombreCurso'=>'Tecnicas de Pedagogia Virtual',
-            'Image_URL'=>'img/Resources/test_img_5.png',
+            'Image_URL'=>'/img/Resources/test_img_5.png',
             'Descripcion'=>'Implementacion de nuevas tecnologias TIC para impartir clases',
             'HorasClase'=>'12',
             'Nivel'=>'Intermedio',
@@ -60,7 +60,7 @@ class CursosSeeder extends Seeder
         
         Curso::create([
             'NombreCurso'=>'Maquinas Virtuales (VWWare Station)',
-            'Image_URL'=>'img/Resources/test_img_6.png',
+            'Image_URL'=>'/img/Resources/test_img_6.png',
             'Descripcion'=>'Esta es una descripcion de prueba',
             'HorasClase'=>'18',
             'Nivel'=>'Principiante',
@@ -69,7 +69,7 @@ class CursosSeeder extends Seeder
         
         Curso::create([
             'NombreCurso'=>'Matematicas Interactivas',
-            'Image_URL'=>'img/Resources/test_img_7.png',
+            'Image_URL'=>'/img/Resources/test_img_7.png',
             'Descripcion'=>'Esta es una descripcion de prueba',
             'HorasClase'=>'50',
             'Nivel'=>'Avanzado',
@@ -78,7 +78,7 @@ class CursosSeeder extends Seeder
         
         Curso::create([
             'NombreCurso'=>'Redaccion tecnica',
-            'Image_URL'=>'img/Resources/test_img_8.png',
+            'Image_URL'=>'/img/Resources/test_img_8.png',
             'Descripcion'=>'Esta es una descripcion de prueba',
             'HorasClase'=>'15',
             'Nivel'=>'Intermedio',
@@ -87,7 +87,7 @@ class CursosSeeder extends Seeder
         
         Curso::create([
             'NombreCurso'=>'Diseños arquitectonicos',
-            'Image_URL'=>'img/Resources/test_img_9.png',
+            'Image_URL'=>'/img/Resources/test_img_9.png',
             'Descripcion'=>'Esta es una descripcion de prueba',
             'HorasClase'=>'10',
             'Nivel'=>'Avanzado',
