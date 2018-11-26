02cbe3ca3bb68800cb8e9d99ddcf0116c1b73545
dfzamora <danielfranciscoz@hotmail.com>
2018-11-26 16:21:55 -0600

    mas cambios en la estructura de la base de datos

--- Posgradophp/database/seeds/DatabaseSeeder.php
+++ Posgradophp/database/seeds/DatabaseSeeder.php
@@ -14,18 +14,26 @@ class DatabaseSeeder extends Seeder
 
         DB::statement('SET FOREIGN_KEY_CHECKS = 0'); //Desactivo la verificacion de llaves foraneas para poder eliminar en caso de que exista relacion entre tablas
         DB::table('categorias')->truncate(); 
-        DB::table('cursos')->truncate(); //Elimina todos los registros de la tabla
-        DB::table('etiquetas')->truncate(); 
-        DB::table('curso_etiqueta')->truncate(); 
         DB::table('comentarios')->truncate(); 
+        DB::table('competencia_cursos')->truncate(); 
+        DB::table('curso_docente')->truncate(); 
+        DB::table('curso_etiqueta')->truncate(); 
+        DB::table('cursos')->truncate(); //Elimina todos los registros de la tabla
+        DB::table('docentes')->truncate(); 
+        DB::table('etiquetas')->truncate(); 
+        DB::table('modalidad_cursos')->truncate(); 
+        DB::table('precio_cursos')->truncate(); 
+        DB::table('requisito_cursos')->truncate(); 
+        DB::table('tematica_cursos')->truncate(); 
         DB::statement('SET FOREIGN_KEY_CHECKS = 1'); //Reactivo la verificacion de llaves foraneas
         
         // $this->call(UsersTableSeeder::class);
-        $this->call(ComentariosSeeder::class);
         $this->call(CategoriasSeeder::class);
+        $this->call(ComentariosSeeder::class);
         $this->call(CursosSeeder::class);
         $this->call(EtiquetasSeeder::class);
         $this->call(Curso_EtiquetaSeeder::class);
+        $this->call(DocentesSeeder::class);
     }
     
     //Para ejecutar los inserts hay que escribir en la linea de comandos
