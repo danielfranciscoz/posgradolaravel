eb8ca4511b5d22b112b686149e44b4041d4dc584
Yasser Radamez Montiel Lozano <yasser.montiel@dtic.uni.edu.ni>
2018-12-17 16:15:35 -0600

    nueva vista resultado

---
+++ Posgradophp/resources/views/Account/registrocompleto.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.app')
+@section('title', 'Registro nuevo usuario')
+@section('content')
+<main class="grey lighten-4">
+    <div class="container wow fadein">
+         <div class="row d-flex justify-content-center">
+            <div class="col-md-8 col-sm-12" >
+                        </br>
+                 <div id="registernow" class="white">
+                 </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+@endsection
