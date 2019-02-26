26d638e522717768eace8c67c70eea0963d94f54
dfzamora <danielfranciscoz@hotmail.com>
2019-02-26 16:47:46 -0600

    mejoras visuales a errores

--- Posgradophp/resources/views/Shared/forbidden.blade.php
+++ Posgradophp/resources/views/Shared/forbidden.blade.php
@@ -1,1 +1,19 @@
-Acceso denegado, Esta página no se encuentra disponible para el público. 
+
+@extends('layout.app')
+@section('title', 'Inicio')
+@section('content')
+
+    <div class="text-center">
+        <img src="{{route('cursos.index')}}/svg/403.svg" style="width:40%"  />
+        <br/>
+        <a class="btn btn-danger btn-sm" onclick='window.location.href = "{{route('cursos.index')}}"'>Ir a inicio</a>
+   
+    </div>
+    
+   <!--  <div class="col-6 ">
+        <div class="col-6 ">
+            <h5>Lo sentimos su dirección de url no </h5>
+        </div>       
+    </div> -->
+
+@endsection
--- Posgradophp/resources/views/Shared/notfound.blade.php
+++ Posgradophp/resources/views/Shared/notfound.blade.php
@@ -1,1 +1,19 @@
-Lo sentimos, la página que busca no ha sido encontrada. 
+
+@extends('layout.app')
+@section('title', 'Inicio')
+@section('content')
+
+    <div class="text-center">
+        <img src="{{route('cursos.index')}}/svg/404.svg" style="width:40%"  />
+        <br/>
+        <a class="btn btn-primary btn-sm" onclick='window.location.href = "{{route('cursos.index')}}"'>Ir a inicio</a>
+   
+    </div>
+    
+   <!--  <div class="col-6 ">
+        <div class="col-6 ">
+            <h5>Lo sentimos su dirección de url no </h5>
+        </div>       
+    </div> -->
+
+@endsection
