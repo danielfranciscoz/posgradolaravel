91ca0b90ff9a0f8c87291d5aaddeb52d1c415222
dfzamora <danielfranciscoz@hotmail.com>
2019-03-11 16:34:18 -0600

    componiendo pago, aun no funciona

--- Posgradophp/app/Http/Controllers/PaymentController.php
+++ Posgradophp/app/Http/Controllers/PaymentController.php
@@ -41,18 +41,13 @@ class PaymentController extends Controller
     {
         $access_key = env('BANK_ACCESS_KEY');
         $profile_id = env('BANK_PROFILE_ID');
-        $transaction_uuid = mt_rand( 1000000, 9999999 );
-        $signed_field_names = 'access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency,payment_method,bill_to_forename,bill_to_surname,bill_to_email,bill_to_phone,bill_to_address_line1,bill_to_address_city,bill_to_address_state,bill_to_address_country,bill_to_address_postal_code';
-        $unsigned_field_names = 'card_type,card_number,card_expiry_date';
-        $signed_date_time = '2019-02-27T20:11:17Z';//date('Y-m-d H:i:s');
-        $locale = 'en';
         $transaction_type = 'authorization';
-        $reference_number = '1551298322957';
+        $reference_number = '1552336803066';
         $amount = '100.00';
         $currency = 'USD';
         $payment_method = 'card';
-        $bill_to_forename = 'daniel';
-        $bill_to_surname = 'daniel';
+        $bill_to_forename = 'John';
+        $bill_to_surname = 'Doe';
         $bill_to_email = 'null@cybersource.com';
         $bill_to_phone = '02890888888';
         $bill_to_address_line1 = '1 Card Lane';
@@ -60,6 +55,12 @@ class PaymentController extends Controller
         $bill_to_address_state = 'CA';
         $bill_to_address_country = 'US';
         $bill_to_address_postal_code = '94043';
+        
+        $transaction_uuid = '5c86c54e0509a';//mt_rand( 1000000, 9999999 );
+        $signed_field_names = 'access_key,profile_id,transaction_uuid,signed_field_names,unsigned_field_names,signed_date_time,locale,transaction_type,reference_number,amount,currency,payment_method,bill_to_forename,bill_to_surname,bill_to_email,bill_to_phone,bill_to_address_line1,bill_to_address_city,bill_to_address_state,bill_to_address_country,bill_to_address_postal_code';
+        $unsigned_field_names = 'card_type,card_number,card_expiry_date';
+        $signed_date_time = '2019-03-11T20:30:06Z';//date('Y-m-d H:i:s');
+        $locale = 'en';
         $submit = 'Submit';
         
         $card_type = '001';
@@ -92,68 +93,99 @@ class PaymentController extends Controller
         );
 
 
-        return response()->json([
-            'access_key'=>$access_key,
-            'profile_id'=>$profile_id ,
-            'transaction_uuid'=>$transaction_uuid ,
-            'signed_field_names'=>$signed_field_names ,
-            'unsigned_field_names'=>$unsigned_field_names,
-            'signed_date_time'=>$signed_date_time,
-            'locale'=>$locale,
-            'transaction_type'=>$transaction_type,
-            'reference_number'=>$reference_number,
-            'amount'=>$amount ,
-            'currency'=>$currency ,
-            'payment_method'=>$payment_method,
-            'bill_to_forename'=>$bill_to_forename ,
-            'bill_to_surname'=>$bill_to_surname ,
-            'bill_to_email'=>$bill_to_email ,
-            'bill_to_phone'=>$bill_to_phone ,
-            'bill_to_address_line1'=>$bill_to_address_line1,
-            'bill_to_address_city'=>$bill_to_address_city,
-            'bill_to_address_state'=>$bill_to_address_state,
-            'bill_to_address_country'=>$bill_to_address_country,
-            'bill_to_address_postal_code'=>$bill_to_address_postal_code,
-            'submit' => 'Submit',
-            'signature' => $signature,
-            'card_type'=>$card_type,
-            'card_number'=>$card_number,
-            'card_expiry_date'=>$card_expiry_date
-        ]);
+        // return response()->json([
+        //     'access_key'=>$access_key,
+        //     'profile_id'=>$profile_id ,
+        //     'transaction_uuid'=>$transaction_uuid ,
+        //     'signed_field_names'=>$signed_field_names ,
+        //     'unsigned_field_names'=>$unsigned_field_names,
+        //     'signed_date_time'=>$signed_date_time,
+        //     'locale'=>$locale,
+        //     'transaction_type'=>$transaction_type,
+        //     'reference_number'=>$reference_number,
+        //     'amount'=>$amount ,
+        //     'currency'=>$currency ,
+        //     'payment_method'=>$payment_method,
+        //     'bill_to_forename'=>$bill_to_forename ,
+        //     'bill_to_surname'=>$bill_to_surname ,
+        //     'bill_to_email'=>$bill_to_email ,
+        //     'bill_to_phone'=>$bill_to_phone ,
+        //     'bill_to_address_line1'=>$bill_to_address_line1,
+        //     'bill_to_address_city'=>$bill_to_address_city,
+        //     'bill_to_address_state'=>$bill_to_address_state,
+        //     'bill_to_address_country'=>$bill_to_address_country,
+        //     'bill_to_address_postal_code'=>$bill_to_address_postal_code,
+        //     'submit' => 'Submit',
+        //     'signature' => $signature,
+        //     'card_type'=>$card_type,
+        //     'card_number'=>$card_number,
+        //     'card_expiry_date'=>$card_expiry_date
+        // ]);
+
+
+        //tc_ni_001287523
+        //VnQNroLhF+XvSNsjDYs9EFKM8+G+e6nd9yougtT85s58aj6UKr+5fjT484H7V+dL+ZEFDoMyimyzKHJiC4+/+6+SLMoS8f1sXefyxP8vH2GCR2fAIvSNGqJky9An7RBpLsQgcu2DzkoJJpiwowz+t90HLcpX+GnkAzeYb1v9mDxASgB6dnO4HKp80umeznwQ8f3t4/oTpmNSzVjjSqC+MGaj7ovTQCSMN/Ot7O2KjOZxrzrW/UOvm8SMwHtFUDV2ok//fCJHFfvhuBcxlbaA/cOkamBUeTTOQBwRkYMBVQNx9wO8T3h4+pjU2c61LM/QJGSGFA8pAuHrn9Dmn/lPxg==
+        //
     
         $client = new Client();
 
-        $response = $client->post('https://testsecureacceptance.cybersource.com/silent/pay', [
-            'headers' => ['Content-Type' => 'text/html;charset=utf-8'],
+        $response = $client->post('https://ics2wsa.ic3.com/commerce/1.x/transactionProcessor', [
+            'headers' => ['Content-Type' => 'text/xml;charset=utf-8'],
             'form_params' => [
-                'access_key'=>$access_key,
-                'profile_id'=>$profile_id ,
-                'transaction_uuid'=>$transaction_uuid ,
-                'signed_field_names'=>$signed_field_names ,
-                'unsigned_field_names'=>$unsigned_field_names,
-                'signed_date_time'=>$signed_date_time,
-                'locale'=>$locale,
-                'transaction_type'=>$transaction_type,
-                'reference_number'=>$reference_number,
-                'amount'=>$amount ,
+                'merchandID'=>'Foster City Flowers' ,
+                'merchantReferenceCode'=>'482046C3A7E94F5BD1' ,
+                'firstName'=>$bill_to_forename ,
+                'lastName'=>$bill_to_surname ,
+                'street1'=>$bill_to_address_line1,
+                'city'=>$bill_to_address_city,
+                'state'=>$bill_to_address_state,
+                'postalCode'=>$bill_to_address_postal_code,
+                'country'=>$bill_to_address_country,
+                'phoneNumber'=>$bill_to_phone ,
+                'email'=>$bill_to_email ,
+                'item id'=>'1',
+                'unitPrice'=>'1',
+                'quantity'=>'1',
                 'currency'=>$currency ,
-                'payment_method'=>$payment_method,
-                'bill_to_forename'=>$bill_to_forename ,
-                'bill_to_surname'=>$bill_to_surname ,
-                'bill_to_email'=>$bill_to_email ,
-                'bill_to_phone'=>$bill_to_phone ,
-                'bill_to_address_line1'=>$bill_to_address_line1,
-                'bill_to_address_city'=>$bill_to_address_city,
-                'bill_to_address_state'=>$bill_to_address_state,
-                'bill_to_address_country'=>$bill_to_address_country,
-                'bill_to_address_postal_code'=>$bill_to_address_postal_code,
-                'submit' => 'Submit',
-                'signature' => $this->sign($signature),
+                'accountNumber'=>$card_number,
+                'expirationMonth'=>'12',
+                'expirationYear'=>'2015',
                 'card_type'=>$card_type,
-                'card_number'=>$card_number,
-                'card_expiry_date'=>$card_expiry_date
-            ],
+                'ccAuthService'=>'true',
+           ],
         ]);
+
+        // $response = $client->post('https://testsecureacceptance.cybersource.com/silent/pay', [
+        //     'headers' => ['Content-Type' => 'text/html;charset=utf-8'],
+        //     'form_params' => [
+        //         'access_key'=>$access_key,
+        //         'profile_id'=>$profile_id ,
+        //         'transaction_uuid'=>$transaction_uuid ,
+        //         'signed_field_names'=>$signed_field_names ,
+        //         'unsigned_field_names'=>$unsigned_field_names,
+        //         'signed_date_time'=>$signed_date_time,
+        //         'locale'=>$locale,
+        //         'transaction_type'=>$transaction_type,
+        //         'reference_number'=>$reference_number,
+        //         'amount'=>$amount ,
+        //         'currency'=>$currency ,
+        //         'payment_method'=>$payment_method,
+        //         'bill_to_forename'=>$bill_to_forename ,
+        //         'bill_to_surname'=>$bill_to_surname ,
+        //         'bill_to_email'=>$bill_to_email ,
+        //         'bill_to_phone'=>$bill_to_phone ,
+        //         'bill_to_address_line1'=>$bill_to_address_line1,
+        //         'bill_to_address_city'=>$bill_to_address_city,
+        //         'bill_to_address_state'=>$bill_to_address_state,
+        //         'bill_to_address_country'=>$bill_to_address_country,
+        //         'bill_to_address_postal_code'=>$bill_to_address_postal_code,
+        //         'submit' => 'Submit',
+        //         'signature' => 'kVNId76NJMwQzxBs4GTOx1nnZQXILD0wMvqwVk8jO9s',//$this->sign($signature),
+        //         'card_type'=>$card_type,
+        //         'card_number'=>$card_number,
+        //         'card_expiry_date'=>$card_expiry_date
+        //     ],
+        // ]);
 
         return response()->json([
             'respuesta'=>$response->getBody()
@@ -161,3 +193,35 @@ class PaymentController extends Controller
         return json_decode($response->getBody())->success;
     }
 }
+
+/*
+// Load the configuration settings
+$config = cybs_load_config( 'cybs.ini' );
+// set up the request by creating an array and adding fields to it
+$request = array();
+// We want to do credit card authorization in this example
+$request['ccAuthService_run'] = "true";
+// Add required fields
+$request['merchantID'] = 'infodev';
+$request['merchantReferenceCode'] = 'MRC-14344';
+$request['billTo_firstName'] = 'Jane';
+$request['billTo_lastName'] = 'Smith';
+$request['billTo_street1'] = '1295 Charleston Road';
+$request['billTo_city'] = 'Mountain View';
+$request['billTo_state'] = 'CA';
+$request['billTo_postalCode'] = '94043';
+$request['billTo_country'] = 'US';
+$request['billTo_email'] = 'jsmith@example.com';
+$request['card_accountNumber'] = '4111111111111111';
+$request['card_expirationMonth'] = '12';
+$request['card_expirationYear'] = '2010';
+$request['purchaseTotals_currency'] = 'USD';
+// This example has two items
+$request['item_0_unitPrice'] = '12.34';
+$request['item_1_unitPrice'] = '56.78';
+// Add optional fields here according to your business needs
+// Send request
+$reply = array();
+$status = cybs_run_transaction( $config, $request, $reply );
+// Handle the reply. See "Handling the Return Status," page 369.
+*/
